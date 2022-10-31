47d5538f8f86149dc32ee44daad08c5fa63518bb
himarnoel <olaimarnoel@gmail.com>
2022-11-01 00:42:43 +0100

    just added btn pix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
-
+import btn from "./assets/btn.png";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <div className="flex bg-white h-screen max-w-[1240px]">
-
-
+      <div className=" bg-green-500 h-screen max-w-[1240px] mx-auto">
+        <img
+          src={btn}
+          alt=""
+          className="object-contain lg:w-12 w-8 float-right mr-10 lg:mr-20 mt-10 "
+        />
+        <img src="" alt="" />
       </div>
     </div>
   );
