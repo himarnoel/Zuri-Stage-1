f15944330ee9b94d88bb01838efb874961cb30a7
himarnoel <olaimarnoel@gmail.com>
2022-11-01 01:22:11 +0100

    working on buttons

--- src/App.jsx
+++ src/App.jsx
@@ -7,36 +7,39 @@ function App() {
   return (
     <div>
       <div className="   max-w-[1240px] lg:mx-auto m-2  lg:mt-0 ">
-        <img
+        <a href="/"> <img
           src={btn}
           alt=""
           className="object-contain lg:w-12 w-8 float-right mr-8  mt-5 lg:mr-20 lg:mt-10 "
-        />
+        /></a>
+       
         <div className="lg:w-44 lg:h-44 h-32 w-32 mx-auto lg:pt-20 pt-10 ">
-          <img src={avatar} alt="" className="object-contain rounded-full" />
+        
+            <img src={avatar} alt="" className="object-contain rounded-full" />
+         
           <p className="text-center font-bold lg:text-xl text-lg ">
             Wizdam Kinm
           </p>
         </div>
-        <div className="lg:mt-44 mt-40 flex flex-col justify-evenly  h-[600px]">
-          <button className="bg-[#EAECF0] py-5  w-full font-semibold">
+        <div className="lg:mt-44 mt-24 flex flex-col justify-evenly  h-[600px]">
+          <a className="bg-[#EAECF0] py-5  w-full font-semibold text-center">
             Twitter Link
-          </button>
-          <button className="bg-[#EAECF0]  py-5  w-full font-semibold">
+          </a>
+          <a className="bg-[#EAECF0]  py-5  w-full font-semibold text-center">
             Zuri Team
-          </button>
-          <button className="bg-[#EAECF0]  py-5  w-full font-semibold">
+          </a>
+          <a className="bg-[#EAECF0]  py-5  w-full font-semibold text-center">
             Zuri Books
-          </button>
-          <button className="bg-[#EAECF0]  py-5  w-full font-semibold">
+          </a>
+          <a className="bg-[#EAECF0]  py-5  w-full font-semibold text-center">
             Python Books
-          </button>
-          <button className="bg-[#EAECF0]  py-5  w-full font-semibold">
+          </a>
+          <a className="bg-[#EAECF0]  py-5  w-full font-semibold text-center">
             Background Check for Coders
-          </button>
-          <button className="bg-[#EAECF0]  py-5  w-full font-semibold">
+          </a>
+          <a className="bg-[#EAECF0]  py-5  w-full font-semibold text-center">
             Design
-          </button>
+          </a>
         </div>
       </div>
     </div>
