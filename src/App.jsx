e1bf0a4b1bee5117337844c62a4c2cfd6ac8c9fb
himarnoel <olaimarnoel@gmail.com>
2022-11-05 11:32:28 +0100

    react router is working fully now

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import "./App.css";
-import { Route, Link, Routes } from "react-router-dom";
+import { Route,Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
