5e0463696728800ea06805e5c233032ce2eab742
himarnoel <olaimarnoel@gmail.com>
2022-11-05 11:38:17 +0100

    contact path was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-
 import "./App.css";
-import { Route,Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+         <div className=" m-3 lg:m-0 lg:w-[1000px] lg:mx-auto   lg:mt-0 ">
+            
+         </div>
+
+    </div>
+  )
+}
+
+export default Contact
