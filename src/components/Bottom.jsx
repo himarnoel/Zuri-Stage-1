e9aa0af32c1b114c0b9f41146675e9b489f726c1
himarnoel <olaimarnoel@gmail.com>
2022-11-05 13:30:15 +0100

    removed bg color in footer in home page

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -3,7 +3,7 @@ import I4g from "../assets/I4G.png";
 import zuri from "../assets/Vector.png";
 const Bottom = () => {
   return (
-    <footer className=" w-full py-6 bg-red-900">
+    <footer className=" w-full py-6 ">
       <div className="h-[1px] mx-auto bg-[#EAECF0] lg:w-[78%] w-[90%]"></div>
       <div className="grid lg:grid-cols-3  mt-5 lg:justify-items-center gap-4 lg:gap-0 ml-2 ">
         <img src={zuri} alt="" className="object-contain" />
